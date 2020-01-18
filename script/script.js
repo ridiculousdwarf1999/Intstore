091d177d149550d978a0cabd560944c547a7de17
Sam Davydov <hildburghauser99@mail.ru>
2020-01-18 17:47:38 +0300

    Вввёл несколько констант, написал функцию, которая добавляет HTML-код дял карточек, добавил в обёртку для всех карточек три карточки с поощью метода appendChild(), закомментировал лишнюю карточку в index.html

--- script/script.js
+++ script/script.js
@@ -1,6 +1,34 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', () => {
 
   const search = document.querySelector('.search')
-  console.log('search: ', search);
+  const cartBtn = document.getElementById('cart');
+  const wishlistBtn = document.getElementById('wishlist');
+  const goodsWrapper = document.querySelector('.goods-wrapper');
+
+  const createCardGoods = (id, title, price, img) => {
+
+    const card = document.createElement('div');
+    card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+    card.innerHTML = `<div class="card">
+                        <div class="card-img-wrapper">
+                          <img class="card-img-top" src="${img}" alt="">
+                          <button class="card-add-wishlist"
+                            data-goods-id="${id}"></button>
+                        </div>
+                        <div class="card-body justify-content-between">
+                          <a href="#" class="card-title">${title}</a>
+                          <div class="card-price">${price} ₽</div>
+                          <div>
+                            <button class="card-add-cart"
+                              data-goods-id="${id}">Добавить в корзину</button>
+                          </div>
+                        </div>
+                      </div>`;
+    return card
+  };
+
+  goodsWrapper.appendChild(createCardGoods(1, 'Дартс', 2000, 'img/temp/Archer.jpg'));
+  goodsWrapper.appendChild(createCardGoods(2, 'Фламинго', 3000, 'img/temp/Flamingo.jpg'));
+  goodsWrapper.appendChild(createCardGoods(3, 'Носки', 333, 'img/temp/Socks.jpg'));
 
 });
